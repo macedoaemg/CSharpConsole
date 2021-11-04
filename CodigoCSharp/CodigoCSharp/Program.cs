18b7e0f9895813ddf06a07a8517f62db8c89e3db
macedoaemg <macedo.anderson@gmail.com>
2021-11-04 11:24:43 -0300

    subindo novo exercício

--- CodigoCSharp/CodigoCSharp/Program.cs
+++ CodigoCSharp/CodigoCSharp/Program.cs
@@ -6,25 +6,16 @@ namespace CodigoCSharp
     {
         static void Main(string[] args)
         {
-            double vh = 0; double qh = 0; double inss = 0; double sb = 0; double td = 0; double sl = 0;
 
-            Console.Write("Quantidade de horas..: ");
-            qh = double.Parse(Console.ReadLine());
+            int contador = 1;
 
-            Console.Write("Valor da hora........: ");
-            vh = double.Parse(Console.ReadLine());
+            while (contador <= 10)
+            {
+                Console.WriteLine("Anderson Macedo");
+                contador = contador + 1;
 
-            //Console.Write("Desconto INSS........: ");
-            //inss = double.Parse(Console.ReadLine());
+            }
 
-            sb = vh * qh;
-            //td =  sb * (inss / 100);
-            sl = sb * 1.09;
-            //sl = sb - td;
-
-            Console.WriteLine("Salário Líquido..: " + sl);
-            if(sl > 1100)            
-                Console.WriteLine("Salário acima do mínimo");
             
             Console.WriteLine("Fim do Programa!");
         }
---
+++ CodigoCSharp/CodigoCSharp/SalarioProfissional.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodigoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vh = 0; double qh = 0; double inss = 0; double sb = 0; double td = 0; double sl = 0;
+
+            Console.Write("Quantidade de horas..: ");
+            qh = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor da hora........: ");
+            vh = double.Parse(Console.ReadLine());
+
+            //Console.Write("Desconto INSS........: ");
+            //inss = double.Parse(Console.ReadLine());
+
+            sb = vh * qh;
+            //td =  sb * (inss / 100);
+            sl = sb * 1.09;
+            //sl = sb - td;
+
+            Console.WriteLine("Salário Líquido..: " + sl);
+            if(sl > 1100)            
+                Console.WriteLine("Salário acima do mínimo");
+            
+            Console.WriteLine("Fim do Programa!");
+        }
+
+    }
+
+}
