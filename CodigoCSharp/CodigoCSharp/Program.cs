4948c0bddef2674967b4ee9fbb0ce25861c189b0
macedoaemg <macedo.anderson@gmail.com>
2021-11-05 11:26:25 -0300

    Versão 3

--- CodigoCSharp/CodigoCSharp/Program.cs
+++ CodigoCSharp/CodigoCSharp/Program.cs
@@ -7,16 +7,65 @@ namespace CodigoCSharp
         static void Main(string[] args)
         {
 
-            int contador = 1;
+            double v1   = 0;
+            double v2   = 0;
+            int    op   = 0;
+            string resp = "S";
 
-            while (contador <= 10)
+            while(resp == "s" || resp == "S")
             {
-                Console.WriteLine("Anderson Macedo");
-                contador = contador + 1;
+                Console.Clear();
+                Console.WriteLine(" ** Menu de Opções ** ");
+                Console.WriteLine("1. Adição");
+                Console.WriteLine("2. Subtração");
+                Console.WriteLine("3. Multiplicação");
+                Console.WriteLine("4. Divisão");
+                Console.Write("Opção..: ");
+                op = int.Parse(Console.ReadLine());                
 
-            }
+                if (op == 1)
+                {
+                    Console.Write("Valor 1..: ");
+                    v1 = double.Parse(Console.ReadLine());
 
-            
+                    Console.Write("Valor 2..: ");
+                    v2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A adição foi de..: " + (v1 + v2));
+                }
+                else if (op == 2)
+                {
+                    Console.Write("Valor 1..: ");
+                    v1 = double.Parse(Console.ReadLine());
+
+                    Console.Write("Valor 2..: ");
+                    v2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A subtração foi de..: " + (v1 - v2));
+                }
+                else if (op == 3)
+                {
+                    Console.Write("Valor 1..: ");
+                    v1 = double.Parse(Console.ReadLine());
+
+                    Console.Write("Valor 2..: ");
+                    v2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A multiplicação foi de..: " + (v1 * v2));
+                }
+                else if (op == 4)
+                {
+                    Console.Write("Valor 1..: ");
+                    v1 = double.Parse(Console.ReadLine());
+
+                    Console.Write("Valor 2..: ");
+                    v2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("A divisão foi de..: " + (v1 / v2));
+                }
+                else
+                    Console.WriteLine("Opção Inválida!!!");
+
+                Console.Write("Deseja executar outra operação <S=SIM>..: ");
+                resp = Console.ReadLine();
+            }                       
+                        
             Console.WriteLine("Fim do Programa!");
         }
 
---
+++ CodigoCSharp/CodigoCSharp/While1Operacao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CodigoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor1 = 0;
+            int valor2 = 0;
+            int soma = 0;
+            
+            string resp = "S";
+            
+            while (resp == "S" || resp == "s")
+            {
+                Console.Clear();
+                Console.Write("Valor 1..: ");
+                valor1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Valor 2..: ");
+                valor2 = int.Parse(Console.ReadLine());
+
+                soma = valor1 + valor2;
+
+                Console.WriteLine("Soma..: " + soma);
+
+                Console.Write("Deseja continuar? <S=Sim>..: ");
+                resp = Console.ReadLine();
+
+                
+            }                
+                        
+            Console.WriteLine("Fim do Programa!");
+        }
+
+    }
+
+}
